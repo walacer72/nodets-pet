02df5afc3020498670ba492abdca8fefff48ca39
Walace Oliveira <walacer72@gmail.com>
2026-01-15 02:00:59 -0300

    base criada

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,19 +1,19 @@
  import { Request, Response } from 'express';
 
  export const home = (req: Request, res: Response) => {
-    // res.render('pages/page');
+   res.send('home no controller');
  }
 
  export const dogs = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    // res.render('pages/dogs');
  }
 
  export const cats = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    // res.render('pages/cats');
  }
 
  export const fishes = (req: Request, res: Response) => {
-    // res.render('pages/page');
+    // res.render('pages/fishes');
  }
 
  
